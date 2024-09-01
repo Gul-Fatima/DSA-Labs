5096c646b3d19ed43122e6fe7621f7cc78e870b7
GUL FATIMA <143262038+Gul-Fatima@users.noreply.github.com>
2024-09-01 07:57:36 -0700

    Update Binary_Search.cpp

--- Dynamic Arrays/Binary_Search.cpp
+++ Dynamic Arrays/Binary_Search.cpp
@@ -61,3 +61,78 @@ int main() {
     delete[] numbers;
     return 0;
 }
+
+
+
+
+#include <iostream>
+//#include <algorithm>
+//
+//using namespace std;
+//
+//int binarySearch(int* nums, int size, int target) {
+//    int left = 0;
+//    int right = size - 1;
+//
+//    while (left <= right) {
+//        int mid = left + (right - left) / 2;
+//
+//        if (nums[mid] == target) {
+//            return mid;
+//        }
+//        else if (nums[mid] < target) {
+//            left = mid + 1;
+//        }
+//        else {
+//            right = mid - 1;
+//        }
+//    }
+//
+//    return -1;
+//}
+//
+//void inputArray(int* arr, int size) {
+//    cout << "Enter " << size << " integers for the array:" << endl;
+//    for (int i = 0; i < size; i++) {
+//        cin >> arr[i];
+//    }
+//}
+//
+//void printArray(int* arr, int size) {
+//    for (int i = 0; i < size; i++) {
+//        cout << arr[i] << " ";
+//    }
+//    cout << endl;
+//}
+//
+//int main() {
+//    int size;
+//    cout << "Enter the size of the array: ";
+//    cin >> size;
+//
+//    int* arr = new int[size];
+//
+//    inputArray(arr, size);
+//
+//    // Sort the array
+//    sort(arr, arr + size);
+//
+//    cout << "Sorted array:" << endl;
+//    printArray(arr, size);
+//
+//    int target;
+//    cout << "Enter the value to search for: ";
+//    cin >> target;
+//
+//    int index = binarySearch(arr, size, target);
+//    if (index != -1) {
+//        cout << "Value found at index: " << index << endl;
+//    }
+//    else {
+//        cout << "Value not found in the array." << endl;
+//    }
+//
+//    delete[] arr;
+//    return 0;
+//}
+
